7eceae1b5597c6b1f50b22af07474fdf99ea75a1
aymen_alnome <ay774953842@gmail.com>
2024-01-29 10:25:13 +0300

    initial commit

---
+++ code/__init__.py
@@ -0,0 +1,11 @@
+def SumAymen(a,b):
+    return a + b 
+
+def SubAymen(a,b):
+    return a - b 
+
+def multAymen(a,b):
+    return a * b 
+
+def divAymen(a,b):
+    return a / b 
---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name='AymenOpreation',
+    version='0.1',
+    author= ' Aymen-Alnomi',
+    description= ' Simple Library in python for test',
+    packages= setuptools.find_packages(),
+    classifiers=[
+        "Programming Lanaguage :: python :: 3 ",
+        "Operating System :: OS Independent ",
+        " License :: OSI Approved :: MIT License",
+    ]
+
+)
